97621f06f8bf184acbb1641c3ae0f1ba9f139e4b
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-12-31 11:49:30 -0500

    added feature cards

--- src/components/FeatureCard.js
+++ src/components/FeatureCard.js
@@ -1,10 +1,49 @@
 import React from "react";
 import styled from "styled-components";
 
-export default function FeatureCard({className, title, icon}) {
+export default function FeatureCard({className, title, children}) {
     return (
-        <div>
+        <Container>
             {/* TODO - Square background with an icon and text underneath */}
-        </div>
+            <Card>
+                {children}
+            </Card>
+            <Title>{title}</Title>
+        </Container>
     )
 }
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: center;
+`
+
+const Card = styled.div`
+    background-color: var(--secondary);
+    border-radius: 10px;
+    width: 166px;
+    height: 166px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const Title = styled.h3`
+    /* Font Formatting */
+    font-family: Montserrat;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 22px;
+    line-height: 27px;
+    text-align: center;
+    letter-spacing: 0.05em;
+
+    color: var(--primary);
+    text-transform: uppercase;
+
+    margin: 0;
+    margin-top: 8px;
+`
--- src/sections/About.js
+++ src/sections/About.js
@@ -1,26 +1,43 @@
 import React from "react";
 import styled from "styled-components";
+import FeatureCard from "../components/FeatureCard";
+
+// Icons
+import { CgBolt } from 'react-icons/cg';
+import { BiDevices } from 'react-icons/bi';
+import { BsLightbulbFill } from 'react-icons/bs';
+import { IoIosRocket } from 'react-icons/io';
 
 export default function About() {
     return (
         <Container>
-            <HeaderText>ABOUT</HeaderText>
+            <HeaderContainer>
+                <HeaderText>ABOUT</HeaderText>
+            </HeaderContainer>
+            
+            <FeaturesContainer>
+                <FeatureCard title="Fast">
+                    <FastIcon />
+                </FeatureCard>
+                <FeatureCard title="Responsive">
+                    <ResponsiveIcon />
+                </FeatureCard>
+                <FeatureCard title="Intuitive">
+                    <IntuitiveIcon />
+                </FeatureCard>
+                <FeatureCard title="Dynamic">
+                    <DynamicIcon />
+                </FeatureCard>
+            </FeaturesContainer>
         </Container>
     )
 }
 
 const HeaderText = styled.h2`
     margin: 0;
+
     /* have the element hug the content of the text */
-    display: inline-block;
-
-    /* Center the header on the page */
-    position: absolute;
-    left: 50%;
-    transform: translateX(-50%);
-
-    /* Make room for the nav bar */
-    margin-top: 125px;
+    display: inline;
 
     /* Text formatting */
     font-family: Montserrat;
@@ -35,11 +52,52 @@ const HeaderText = styled.h2`
 
     /* Stylized underline */
     border-bottom: 5px solid var(--primary);
-    
 `
 
 const Container = styled.div`
     /* Compensates for header centering technique */
     width: 100%;
     overflow: hidden;
+`
+
+const FeaturesContainer = styled.div`
+    margin-left: 196px; margin-right: 196px;
+    margin-top: 64px;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`
+
+const HeaderContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    /* Make room for the nav bar */
+    margin-top: 125px;
+`
+
+const FastIcon = styled(CgBolt)`
+    color: #ffffff;
+    width: 96px;
+    height: 96px;
+`
+
+const ResponsiveIcon = styled(BiDevices)`
+    color: #ffffff;
+    width: 96px;
+    height: 96px;
+`
+
+const IntuitiveIcon = styled(BsLightbulbFill)`
+    color: #ffffff;
+    width: 96px;
+    height: 96px;
+`
+
+const DynamicIcon = styled(IoIosRocket)`
+    color: #ffffff;
+    width: 96px;
+    height: 96px;
 `
